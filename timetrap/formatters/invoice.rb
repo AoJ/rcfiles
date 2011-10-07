a0828ce5c26e3f8e88c3a3a9be0d4f25f735bb4c
Carl Sverre <carl@carlsverre.com>
2011-10-06 17:18:03 -0700

    Added basic html invoice formatter for timetrap

---
+++ timetrap/formatters/invoice.rb
@@ -0,0 +1,40 @@
+require 'sinatra/base'
+require 'pp'
+
+class InvoiceServer < Sinatra::Base
+  set :entries, []
+
+  get "/" do
+    @rate = settings.rate.to_i
+    @bille_name = settings.bille_name
+    @bille_company = settings.bille_company
+    @entries = settings.entries
+
+    erb :index
+  end
+end
+
+class Timetrap::Formatters::Invoice
+
+  def initialize(entries)
+    @entries = entries
+  end
+
+  def output
+    "running server on localhost:8080"
+
+    current_sheet = @entries[0][:sheet]
+    data_sheet = Timetrap::Entry.sheets.find_all { |i| i =~ /^#{current_sheet}.+/ }
+   
+    return "You need to create a data sheet first" unless data_sheet.length
+
+    data = data_sheet[0].split(',')
+
+    InvoiceServer.set(:rate, data[1])
+    InvoiceServer.set(:bille_name, data[2])
+    InvoiceServer.set(:bille_company, data[3])
+    InvoiceServer.set(:entries, @entries)
+
+    InvoiceServer.run!
+  end
+end
